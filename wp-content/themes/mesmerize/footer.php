80103a0e171373728d10ae8595e720c68b9b4ba8
Vicroni <drasa_tec@hotmail.com>
2018-11-22 19:15:19 -0600

    Pagina del club de upiita

---
+++ wp-content/themes/mesmerize/footer.php
@@ -0,0 +1,16 @@
+<div class="footer footer-simple">
+    <div class="footer-content center-xs">
+        <div class="gridContainer">
+	        <div class="row middle-xs footer-content-row">
+	            <div class="footer-content-col col-xs-12">
+						<p class="copyright">
+							©&nbsp;&nbsp;2018&nbsp;UPIITA.&nbsp; Blockchain 
+						</p>	           
+			    </div>
+	        </div>
+	    </div>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
